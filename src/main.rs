e2a9b4aff30d2ccb5bb177c2adfb2b4c6cc4e03a
= <=>
2023-05-10 16:54:44 +0000

    waldir dep, stupid attempt at making the actual code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use std::env;
+use walkdir::DirEntry;
+use walkdir::WalkDir;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut bx: Box<[u8; 19*19]> = Box::from([b'e'; 19*19]);
+    bx[20] = b'w';
+    println!("Here is it: {}", bx[21] as char);
+    let args: Vec<String> = env::args().collect();
+    let input_directory = &args[1];
+    //let output_directory = &args[2];
+    let walker = WalkDir::new(input_directory).into_iter();
+    let mut i = 1;
+    for entry in walker {
+        let entry = entry.unwrap();
+        if is_sgf(&entry) {
+            let mut EV: &str = "NaN";
+            let mut PB: &str;
+            let mut PW: &str;
+            let mut WR: &str;
+            let mut KM: f32;
+            let mut RE: &str;
+            let mut DT: &str;
+            let mut black_moves: Vec<[u8; 2]>;
+            let mut white_moves: Vec<[u8; 2]>;
+            let contents = fs::read_to_string(entry.path()).expect("Weird, you should have all access you need.");
+            println!("Here is kifu: \n\n{contents}");
+            break
+        }
+    }
+}
+
+fn move_add(mut board: Box<[&u8; 19*19]>, mv: &[u8; 2], is_black: bool) {
+    let x = mv[0] - 96;
+    let y = mv[1] - 96;
+    let index: usize = x as usize * 19 + y as usize;
+    *board[index] = if is_black {b'w'} else {b'b'};
+}
+
+fn is_sgf(entry: &DirEntry) -> bool {
+    entry.file_name()
+         .to_str()
+         .map(|s| s.ends_with(".sgf"))
+         .unwrap_or(false)
 }
